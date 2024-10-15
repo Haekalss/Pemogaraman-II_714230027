2fa8d13712f5ec435420312c0e52c4f0b5820262
Haekalss <kallsss145@gmail.com>
2024-10-15 16:10:02 +0700

    praktikum 2

---
+++ Pertemuan02/Praktikum/P2_1_714230027/P2_1_714230027/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230027
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("angka pertama = ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("angka kedua = ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(input + "+" + input2 + "=" + (input + input2));
+			Console.WriteLine(input + "-" + input2 + "=" + (input - input2));
+			Console.WriteLine(input + "*" + input2 + "=" + (input * input2));
+			Console.WriteLine(input + "/" + input2 + "=" + (input / input2));
+		}
+    }
+}
